19d8063635b6704b389039a6508283dc246212d1
Vitya Chepovoi <vchepovoi@gmail.com>
2020-02-20 17:03:35 +0300

    feat: decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array.length == 0 && !array) {
+    return 0;
+  }  else {
+    var minValue = array[0];
+    for (let i = 1 ; i < array.length ; i++) {
+      if (minValue > array[i]) {
+        minValue = array[i];
+      }
+    }
+    return minValue;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) {
+    return 0;
+  }  else {
+    var maxValue = array[0];
+    for (let i = 1 ; i < array.length ; i++) {
+      if (maxValue < array[i]) {
+        maxValue = array[i];
+      }
+    }
+    return maxValue;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) {
+    return 0;
+  }  else {
+    var sum = 0;
+    var averageValue;
+    for (let i = 0 ; i < array.length ; i++) {
+    sum += array[i];
+    averageValue = sum / array.length;
+    }
+    return averageValue;
+  }
 }
