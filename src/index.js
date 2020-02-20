6147d324dd326dcfdec35c923a11802179c4cbaf
Vitya Chepovoi <vchepovoi@gmail.com>
2020-02-20 19:07:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,41 +1,53 @@
 exports.min = function min (array) {
-  if (array.length == 0 && !array) {
+  if (array === undefined) {
     return 0;
   }  else {
-    var minValue = array[0];
-    for (let i = 1 ; i < array.length ; i++) {
-      if (minValue > array[i]) {
-        minValue = array[i];
+      if (array.length == 0) {
+        return 0;
+      } else {
+          var minValue = array[0];
+          for (let i = 1 ; i < array.length ; i++) {
+            if (minValue > array[i]) {
+            minValue = array[i];
+          }
+        }
       }
+      return minValue;
     }
-    return minValue;
-  }
 }
 
 exports.max = function max (array) {
   if (array === undefined) {
     return 0;
   }  else {
-    var maxValue = array[0];
-    for (let i = 1 ; i < array.length ; i++) {
-      if (maxValue < array[i]) {
-        maxValue = array[i];
+      if (array.length == 0) {
+        return 0;
+      } else {
+          var maxValue = array[0];
+          for (let i = 1 ; i < array.length ; i++) {
+            if (maxValue < array[i]) {
+            maxValue = array[i];
+          }
+        }
       }
+      return maxValue;
     }
-    return maxValue;
-  }
 }
 
 exports.avg = function avg (array) {
-  if (array == undefined) {
+  if (array === undefined) {
     return 0;
   }  else {
-    var sum = 0;
-    var averageValue;
-    for (let i = 0 ; i < array.length ; i++) {
-    sum += array[i];
-    averageValue = sum / array.length;
+      if (array.length == 0) {
+        return 0;
+      } else {
+        var sum = 0;
+        var averageValue;
+        for (let i = 0 ; i < array.length ; i++) {
+        sum += array[i];
+        averageValue = sum / array.length;
+        }
+        return averageValue;
+      }
     }
-    return averageValue;
-  }
 }
